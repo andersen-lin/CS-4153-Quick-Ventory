cbbdfdf29a0ba8b6b9bce4f148fdef9050f7aa3a
Andersen Lin <andersen1997801@gmail.com>
2020-04-04 02:24:18 -0500

    Initial Commit

---
+++ CS-4153-Quick-Ventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS-4153-Quick-Ventory
+//
+//  Created by Andersen Lin on 4/4/20.
+//  Copyright © 2020 Oklahoma State University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
