3bc3347cab67adc46feb592a1013e159e3312cfc
Nathan Zimmer <Nathaniel.zimmer@okstate.edu>
2020-04-10 18:29:00 -0500

    Added User and Rest Registration

---
+++ CS-4153-Quick-Ventory/SignUpRestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpRestViewController.swift
+//  CS-4153-Quick-Ventory
+//
+//  Created by Nathan Zimmer on 4/10/20.
+//  Copyright © 2020 Oklahoma State University. All rights reserved.
+//
+
+import UIKit
+
+class SignUpRestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
