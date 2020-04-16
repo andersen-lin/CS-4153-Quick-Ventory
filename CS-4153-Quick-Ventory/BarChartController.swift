e6be583e9d1ec9d07ef3cd428c72437fe5add254
Jacob <Mathews>
2020-04-16 08:37:49 -0500

    Added Bar and Pie Charts with Dummy Data

---
+++ CS-4153-Quick-Ventory/BarChartController.swift
@@ -0,0 +1,42 @@
+//
+//  BarChartController.swift
+//  CS-4153-Quick-Ventory
+//
+//  Created by jacob mathews on 4/16/20.
+//  Copyright © 2020 Oklahoma State University. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class BarChartController: UIViewController, ChartViewDelegate{
+
+    var barChart = BarChartView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        barChart.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        barChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        
+        barChart.center = view.center
+        view.addSubview(barChart)
+        
+        var entries = [BarChartDataEntry]()
+        
+        for x in 0..<10 {
+            entries.append(BarChartDataEntry(x: Double(x), y: Double(x)))
+        }
+        
+        let set = BarChartDataSet(entries: entries)
+        set.colors = ChartColorTemplates.joyful()
+        
+        let data = BarChartData(dataSet: set)
+        barChart.data = data
+    }
+
+}
---
+++ CS-4153-Quick-Ventory/CircleChartController.swift
@@ -0,0 +1,45 @@
+//
+//  CircleChartController.swift
+//  CS-4153-Quick-Ventory
+//
+//  Created by jacob mathews on 4/16/20.
+//  Copyright © 2020 Oklahoma State University. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class CircleChartController: UIViewController, ChartViewDelegate {
+
+   var pieChart = PieChartView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pieChart.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        pieChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
+        
+        pieChart.center = view.center
+        view.addSubview(pieChart)
+        
+        var entries = [PieChartDataEntry]()
+        
+        for x in 0..<10 {
+            entries.append(PieChartDataEntry(value: Double(x), data: Double(x)))
+        }
+        
+        let set = PieChartDataSet(entries: entries)
+        set.colors = ChartColorTemplates.joyful()
+        
+        let data = PieChartData(dataSet: set)
+        pieChart.data = data
+    }
+    
+
+
+
+}
