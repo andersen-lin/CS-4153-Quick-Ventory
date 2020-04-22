d5237462f97e8e659837cea4f494c9de094d7a34
Andersen Lin <andersen1997801@gmail.com>
2020-04-22 00:10:54 -0500

    Fetched and displayed item names from Item entity on the 3rd table view based on sub_catagory. Next step is to fetch and display data via itemToStock on modify view.

--- CS-4153-Quick-Ventory/ProductController.swift
+++ CS-4153-Quick-Ventory/ProductController.swift
@@ -11,39 +11,10 @@ import CoreData
 
 class ProductController: UITableViewController {
 
+    var dataSource: [NSManagedObject] = []
+    
     var appDelegate: AppDelegate?
     var context: NSManagedObjectContext?
-    var itemEntity: NSEntityDescription?
-    
-    let breadsSource = ["White", "Wheat"]
-    let meatsSource = ["Beef", "Pork", "Chicken"]
-    let cheesesSource = ["American", "Cheddar", "Parmesan", "Swiss"]
-    let vegetablesSource = ["Spinach", "Lettuce", "Cucumber"]
-    let condimentsSource = ["Mustard", "Mayonaisse", "Ketchup"]
-    
-    let utensilsSource = ["Spoon", "Knife", "Fork"]
-    let conplSource = ["Plate", "Bowl", "To-Go Box"]
-    let cupsSource = ["Coffee Cup", "Soda Cup"]
-    let napsauSource = ["Napkin", "Sauce Packet"]
-    
-    let chemSource = ["Detergent", "Sanitizer", "Airspray"]
-    let toilSource = ["Soap", "Toothbrush", "Towel"]
-    let cleanSource = ["Broom", "Mop", "Bucket"]
-    
-    let breadsImage = [UIImage(named: "white_bread"), UIImage(named: "wheat_bread")]
-    let meatsImage = [UIImage(named: "beef_meat"), UIImage(named: "pork_meat"), UIImage(named: "chicken_meat")]
-    let cheeseImage = UIImage(named: "cheeses")
-    let vegetablesImage = [UIImage(named: "spinach_vegetable"), UIImage(named: "lettuce_vegetable"), UIImage(named: "cucumber_vegetable")]
-    let condimentsImage = [UIImage(named: "mustard_condiment"), UIImage(named: "mayonaisse_condiment"), UIImage(named: "ketchup_condiment")]
-    
-    let utensilsImage = [UIImage(named: "spoon_utensil"), UIImage(named: "knife_utensil"), UIImage(named: "fork_utensil")]
-    let conplImage = [UIImage(named: "plate_conpl"), UIImage(named: "bowl_conpl"), UIImage(named: "takeaway_conpl")]
-    let cupsImage = [UIImage(named: "coffee_cup"), UIImage(named: "soda_cup")]
-    let napsauImage = [UIImage(named: "napkins"), UIImage(named: "saucepackets")]
-    
-    let chemImage = [UIImage(named: "detergent_chem"), UIImage(named: "sanitizer_chem"), UIImage(named: "airspray_chem")]
-    let toilImage = [UIImage(named: "soap_toil"), UIImage(named: "toothbrush_toil"), UIImage(named: "towel_toil")]
-    let cleanImage = [UIImage(named: "broom_clean"), UIImage(named: "mop_clean"), UIImage(named: "bucket_clean")]
     
     var passedCategory: String!
     var productToPass: String!
@@ -57,7 +28,6 @@ class ProductController: UITableViewController {
         
         appDelegate = UIApplication.shared.delegate as? AppDelegate
         context = appDelegate?.persistentContainer.viewContext
-        itemEntity = NSEntityDescription.entity(forEntityName: "item", in:context!)
         
         self.tableView.register(ProductCell.self, forCellReuseIdentifier: "ProductCell")
         
@@ -72,88 +42,66 @@ class ProductController: UITableViewController {
 
     // MARK: - Table view data source
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Fetch the database contents
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
+        let predicate: NSPredicate
+        
+        switch (passedCategory) {
+        case "Breads":
+            predicate = NSPredicate(format: "sub_category == \"breads\"")
+        case "Meats":
+            predicate = NSPredicate(format: "sub_category == \"meats\"")
+        case "Cheeses":
+            predicate = NSPredicate(format: "sub_category == \"cheeses\"")
+        case "Vegetables":
+            predicate = NSPredicate(format: "sub_category == \"vegetables\"")
+        case "Condiments":
+            predicate = NSPredicate(format: "sub_category == \"condiments\"")
+        case "Utensils":
+            predicate = NSPredicate(format: "sub_category == \"utensils\"")
+        case "Containers and Plates":
+            predicate = NSPredicate(format: "sub_category == \"containers and plates\"")
+        case "Cups":
+            predicate = NSPredicate(format: "sub_category == \"cups\"")
+        case "Napkins and Sauce Packets":
+            predicate = NSPredicate(format: "sub_category == \"napkins and sauce packs\"")
+        case "Chemicals":
+            predicate = NSPredicate(format: "sub_category == \"chemicals\"")
+        case "Toiletries":
+            predicate = NSPredicate(format: "sub_category == \"toiletries\"")
+        case "Cleaning Tools":
+            predicate = NSPredicate(format: "sub_category == \"cleaning tools\"")
+        default:
+            return
+        }
+        fetchRequest.predicate = predicate
+        
+        do {
+            dataSource = try context?.fetch(fetchRequest) ?? []
+        } catch let error as NSError {
+            print("Cannot load data: \(error)")
+        }
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         // #warning Incomplete implementation, return the number of rows
-        switch passedCategory {
-        case "Breads":
-            return breadsSource.count
-        case "Meats":
-            return meatsSource.count
-        case "Cheeses":
-            return cheesesSource.count
-        case "Vegetables":
-            return vegetablesSource.count
-        case "Condiments":
-            return condimentsSource.count
-        case "Utensils":
-            return utensilsSource.count
-        case "Containers and Plates":
-            return conplSource.count
-        case "Cups":
-            return cupsSource.count
-        case "Napkins and Sauce Packets":
-            return napsauSource.count
-        case "Chemicals":
-            return chemSource.count
-        case "Toiletries":
-            return toilSource.count
-        case "Cleaning Tools":
-            return cleanSource.count
-            
-        default:
-            return breadsSource.count
-        }
+        return dataSource.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as! ProductCell
-
+        
         // Configure the cell...
-        switch passedCategory {
-        case "Breads":
-            cell.ProductLabel?.text =  breadsSource[indexPath[1]]
-            cell.ProductImage?.image = breadsImage[indexPath[1]]
-        case "Meats":
-            cell.ProductLabel?.text =  meatsSource[indexPath[1]]
-            cell.ProductImage?.image = meatsImage[indexPath[1]]
-        case "Cheeses":
-            cell.ProductLabel?.text =  cheesesSource[indexPath[1]]
-            cell.ProductImage?.image = cheeseImage
-        case "Vegetables":
-            cell.ProductLabel?.text =  vegetablesSource[indexPath[1]]
-            cell.ProductImage?.image = vegetablesImage[indexPath[1]]
-        case "Condiments":
-            cell.ProductLabel?.text =  condimentsSource[indexPath[1]]
-            cell.ProductImage?.image = condimentsImage[indexPath[1]]
-        case "Utensils":
-            cell.ProductLabel?.text =  utensilsSource[indexPath[1]]
-            cell.ProductImage?.image = utensilsImage[indexPath[1]]
-        case "Containers and Plates":
-            cell.ProductLabel?.text =  conplSource[indexPath[1]]
-            cell.ProductImage?.image = conplImage[indexPath[1]]
-        case "Cups":
-            cell.ProductLabel?.text =  cupsSource[indexPath[1]]
-            cell.ProductImage?.image = cupsImage[indexPath[1]]
-        case "Napkins and Sauce Packets":
-            cell.ProductLabel?.text =  napsauSource[indexPath[1]]
-            cell.ProductImage?.image = napsauImage[indexPath[1]]
-        case "Chemicals":
-            cell.ProductLabel?.text =  chemSource[indexPath[1]]
-            cell.ProductImage?.image = chemImage[indexPath[1]]
-        case "Toiletries":
-            cell.ProductLabel?.text =  toilSource[indexPath[1]]
-            cell.ProductImage?.image = toilImage[indexPath[1]]
-        case "Cleaning Tools":
-            cell.ProductLabel?.text =  cleanSource[indexPath[1]]
-            cell.ProductImage?.image = cleanImage[indexPath[1]]
-        default:
-            cell.ProductLabel?.text =  "N/A"
-        }
+        cell.ProductLabel?.text = dataSource[indexPath[1]].value(forKey: "name") as? String
+        
         return cell
     }
 
